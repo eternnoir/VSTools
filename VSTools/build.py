126775613aaa6d5dfb5b417ad42e27555a7cc74c
eternnoir <eternnoir@gmail.com>
2015-06-05 17:29:45 +0800

    Fix paras error.

--- VSTools/build.py
+++ VSTools/build.py
@@ -4,6 +4,7 @@ __author__ = 'eternnoir'
 
 import os
 from subprocess import Popen, PIPE
+import subprocess
 
 
 class MsBuild:
@@ -11,10 +12,13 @@ class MsBuild:
         self.executePath = msBuildPath
         self.debug = debug
 
-    def build(self, slnPath, *para):
+    def build(self, slnPath, *paras):
         if not os.path.isfile(self.executePath):
             raise Exception("MsBuild.exe not found at " + self.executePath)
-        p = Popen([self.executePath, slnPath, para], stdout=PIPE, stderr=PIPE)
+        command = [self.executePath, slnPath]
+        for par in paras:
+            command.append(par)
+        p = Popen(command, stdout=PIPE, stderr=PIPE)
         output, err = p.communicate()
         if self.debug:
             print(output)
