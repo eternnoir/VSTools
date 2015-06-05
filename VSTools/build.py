9bfc760ef772a626afab792f8ee65b7f370c87a2
eternnoir <eternnoir@gmail.com>
2015-06-05 15:42:21 +0800

    Add debug flag.

--- VSTools/build.py
+++ VSTools/build.py
@@ -5,7 +5,7 @@ import subprocess
 __author__ = 'eternnoir'
 
 import os
-
+from subprocess import Popen, PIPE
 
 class MsBuild:
     def __init__(self, msBuildPath, debug=False):
@@ -15,9 +15,10 @@ class MsBuild:
     def build(self, slnPath, *para):
         if not os.path.isfile(self.executePath):
             raise Exception("MsBuild.exe not found at " + self.executePath)
-        print(self.executePath)
-        print(slnPath)
-        p = subprocess.call([self.executePath, slnPath, para])
-        if p == 1:
+        p = Popen([self.executePath, slnPath, para], stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate()
+        if self.debug:
+            print(output)
+        if p.returncode == 1:
             return False
         return True
