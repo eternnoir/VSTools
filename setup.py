565ff051cabe9eaec6f24df6e8c31115e0a4eed8
eternnoir <eternnoir@gmail.com>
2015-06-05 15:47:19 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='VSTools',
+      version='0.1',
+      description='Easy use Visual Studio msbuild with python. ',
+      author='eternnoir',
+      author_email='eternnoir@gmail.com',
+      url='https://github.com/eternnoir/VSTools',
+      packages=['VSTools'],
+      )
