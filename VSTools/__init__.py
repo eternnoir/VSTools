dbe3d2969012d59ee380e06986daacae54ee4366
eternnoir <eternnoir@gmail.com>
2015-06-05 15:24:27 +0800

    Add build.

--- VSTools/__init__.py
+++ VSTools/__init__.py
@@ -1,5 +1,10 @@
+from VSTools.build import MsBuild
+
 __author__ = 'eternnoir'
 VERSION = "v0.0.1"
 
-FrameworkVersionDic = {"v4.0.30319": "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319"}
+FrameworkVersionDic = {"v4.0.30319":  r'C:\Windows\Microsoft.NET\Framework\v4.0.30319'}
 
+
+def getDefautlMsBuild(debug=False):
+    return MsBuild(FrameworkVersionDic["v4.0.30319"]+r'\MSBuild.exe',debug)
--- VSTools/build.py
+++ VSTools/build.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import subprocess
+
 __author__ = 'eternnoir'
 
+import os
+
+
+class MsBuild:
+    def __init__(self, msBuildPath, debug=False):
+        self.executePath = msBuildPath
+        self.debug = debug
+
+    def build(self, slnPath, *para):
+        if not os.path.isfile(self.executePath):
+            raise Exception("MsBuild.exe not found at " + self.executePath)
+        print(self.executePath)
+        print(slnPath)
+        p = subprocess.call([self.executePath, slnPath, para])
+        if p == 1:
+            return False
+        return True
