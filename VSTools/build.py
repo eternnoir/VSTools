cfce4c1b0cf3bc32b2a116654df9cd5ea44fb40d
eternnoir <eternnoir@gmail.com>
2015-06-11 10:44:00 +0800

    Update.

--- VSTools/build.py
+++ VSTools/build.py
@@ -5,6 +5,7 @@ __author__ = 'eternnoir'
 import os
 from subprocess import Popen, PIPE
 
+
 class MsBuild:
     def __init__(self, msBuildPath, debug=False):
         self.executePath = msBuildPath
@@ -23,8 +24,14 @@ class MsBuild:
         if p.returncode == 1:
             return False
         return True
+    def build_release(self, sln_path):
+        return self.build_release_target(sln_path)
 
-    def build_release(self, slnPath):
+    def build_release_target(self, sln_path, target=None):
         arg1 = '/t:Rebuild'
         arg2 = '/p:Configuration=Release'
-        return self.build(slnPath, arg1, arg2)
+        if target is None:
+            return self.build(sln_path, arg1, arg2)
+        else:
+            arg3 = '/t:' + target
+            return self.build(sln_path, arg1, arg2, arg3)
