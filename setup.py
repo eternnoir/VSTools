3777eb88b686cdb11dc09e50ad53a190f7a79de3
eternnoir <eternnoir@gmail.com>
2016-01-27 15:53:37 +0800

    Update version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='VSTools',
-      version='0.1',
+      version='0.1.1',
       description='Easy use Visual Studio msbuild with python. ',
       author='eternnoir',
       author_email='eternnoir@gmail.com',
